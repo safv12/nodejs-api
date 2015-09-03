d51abe2b3fbbf4241a8a2f145ab7bf0361085a05
safv12 <ing.fran12@gmail.com>
2015-09-02 21:09:42 -0600

    server configs

---
+++ server/config/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  env: {
+    base: 'http://localhost',
+    port: 9000
+  },
+  db: {
+    host: 'localhost',
+    dbName: 'example-db',
+    username: 'nodeapi',
+    password: 'A3TVSGx4Q424DyWf'
+  }
+};
---
+++ server/routes.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function (app) {
+  // app.use('/api/v1/users', require('./api/controllers/users'));
+};
